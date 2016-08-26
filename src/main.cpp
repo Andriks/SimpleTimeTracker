a28dc6e3fb853bba6f85953965a3cd6349dedb63
Andrii Shcherba <AShcherba@intropro.com>
2016-08-26 13:06:16 +0300

    simple make system ready

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 
 
 int main(int argc, char **argv) {
+	std::cout << "programm started" << std::endl;
+
 	AppChangeEventDriver driver;
 	driver.start();
 
+	std::cout << "programm ended" << std::endl;
 	return 0;
 }
