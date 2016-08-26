2df07a41f8d7b3c8362383aa8b9f4727efdc640c
Andrii Shcherba <AShcherba@intropro.com>
2016-08-26 17:35:48 +0300

    [+] event loop to AppChangeEventDriver

--- src/AppChangeEventDriver.cpp
+++ src/AppChangeEventDriver.cpp
@@ -11,18 +11,32 @@ AppChangeEventDriver::~AppChangeEventDriver() {}
 
 void AppChangeEventDriver::start() {
     std::cout << "[beg] AppChangeEventDriver::start()" << std::endl;
+        
+        std::string pid_cache;
+        std::string title_cache;
+        std::string appname_cache;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    // run event loop
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    std::string title = exec_cmd("xdotool getwindowfocus getwindowname");
-    std::string pid = exec_cmd("xdotool getactivewindow getwindowpid");
+        std::string pid = exec_cmd("xdotool getactivewindow getwindowpid");
+        std::string title = exec_cmd("xdotool getwindowfocus getwindowname");
 
-    std::string app_name_request = "ps -p " + pid + " -o comm=";
-    std::string app_name = exec_cmd(app_name_request.c_str());
-    
-    std::cout << "title    --> " << title << std::endl
-              << "app name --> " << app_name << std::endl
-              << "pid      --> " << pid << std::endl;
+        std::string appname_request = "ps -p " + pid + " -o comm=";
+        std::string appname = exec_cmd(appname_request.c_str());
+
+        if (pid_cache != pid) {
+            pid_cache = pid;
+            title_cache = title;
+            appname_cache = appname;
+
+            std::cout << "[pid:" << pid << "] --> " << appname << std::endl;
+            std::cout << title << std::endl;
+            std::cout << "=================================" << std::endl;
+
+        }
+    }
 
     std::cout << "[end] AppChangeEventDriver::start()" << std::endl;
 }
