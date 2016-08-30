c60f3575b9925d2fbcee6eeeb32800de222ce560
Andrii Shcherba <AShcherba@intropro.com>
2016-08-30 13:34:37 +0300

    [+] base input args handling, [+] run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+
+# run tracking
+./bin/simpleTimeTrackerEXE $@
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,19 @@
 
 
 int main(int argc, char **argv) {
-    aced::AppChangeEventDriver driver;
-    driver.start();
+    if (argc > 1) {
+        std::string cmd = argv[1];
+        if (cmd == "run") {
+            aced::AppChangeEventDriver driver;
+            driver.start();
+        } else if (cmd == "report") {
+            std::cout << "some report here" << std::endl;
+        } else {
+            std::cout << "unknown command" << std::endl;
+        }
+    } else {
+        std::cout << "no arguments" << std::endl;
+    }
 
     return 0;
 }
