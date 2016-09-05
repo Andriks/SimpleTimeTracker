ab9a875eed53f68261a1f36720af41d7ecb0c12e
Andrii Shcherba <AShcherba@intropro.com>
2016-09-05 19:37:21 +0300

    [*] improved run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 
-# run tracking
+USAGE=(
+    "USAGE: run.sh (start | stop | report <SomeQuery> | help)",
+    "       start  - will start server for collecting data",
+    "       stop   - will stop server",
+    "       report - will print the result of query processing to the current terminal session",
+    "       help   - will print valid cmd list",
+)
+
+if [ "$1" = "-h" -o "$1" = "help" ]; then
+    printf '%s\n' "${USAGE[@]}"
+    exit
+fi
+
 ./bin/simpleTimeTrackerEXE $@
