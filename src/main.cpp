9c3e4a6c6d23f3b6de5a678da36d91039d456138
Andrii Shcherba <AShcherba@intropro.com>
2016-09-05 18:21:35 +0300

    [+] simple deamon

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 
 #include "AppChangeEventDriver.h"
 
@@ -8,7 +9,22 @@ int main(int argc, char **argv) {
         std::string cmd = argv[1];
         if (cmd == "run") {
             aced::AppChangeEventDriver driver;
-            driver.start();
+            pid_t pid;
+
+            pid = fork();
+            switch (pid) {
+            case -1:
+                std::cerr << "can't make fork" << std::endl;
+                exit(1);
+            case 0:
+                std::cout << "===> STT SERVER: deamon started!" << std::endl
+                          << "===> STT SERVER: deamon PID " << getpid() << " <===" << std::endl;
+
+                driver.start();
+            default:
+                // will just end parent process
+                exit(0);
+            }
         } else if (cmd == "report") {
             std::cout << "some report here" << std::endl;
         } else {
